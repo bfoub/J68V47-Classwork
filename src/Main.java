6d6c76c3c506f3093330a515db8e3b083fdddbac
30044429 <30044429@nescol.ac.uk>
2023-08-30 15:34:43 +0100

    Anagrams and Haiku

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        String string1 = "Blazing ball of fire";
+        String string2 = "Your heat makes the flowers bloom";
+        String string3 = "Thanks for the sunshine";
 
-        System.out.println("Hello World!");
+        System.out.println("Haiku example: ");
+        System.out.println(string1);
+        System.out.println(string2);
+        System.out.println(string3);
 
+        System.out.println();
+
+        System.out.println("5 letter 'Space' anagrams: ");
+        System.out.println("Capes");
+        System.out.println("Paces");
+        System.out.println("Scape");
     }
 }
