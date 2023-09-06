8b05450cee6c343603efbe1ab25b5561d060021e
30044429 <30044429@nescol.ac.uk>
2023-09-06 15:47:52 +0100

    update exercise 3

--- src/L2E3.java
+++ src/L2E3.java
@@ -23,13 +23,13 @@ public class L2E3 {
         System.out.print("Enter the price of the item(3): ");
         Double itemPrice3 = input2.nextDouble();
 
-        Double totalCost = (itemPrice1 + itemPrice2 + itemPrice3);
+        Double subtotal = (itemPrice1 + itemPrice2 + itemPrice3);
 
         System.out.println("Receipt: ");
         System.out.format("%s, £%.2f", itemName1, itemPrice1);
         System.out.format("\n%s, £%.2f", itemName2, itemPrice2);
         System.out.format("\n%s, £%.2f", itemName3, itemPrice3);
-        System.out.format("\ntotal cost: £%.2f", totalCost);
+        System.out.format("\nSUBTOTAL: £%.2f", subtotal);
 
     }
 }
