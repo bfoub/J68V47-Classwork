d334dd66ae73997aed3859d20b0c4cfb401c146b
30044429 <30044429@nescol.ac.uk>
2023-09-06 15:46:59 +0100

    update exercise 1

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -18,10 +18,5 @@ public class Lesson2
         System.out.format("%s loves %s in their free time. ", name, hobby);
         System.out.format("%s provides %s with a lot of joy and a way to be creative. ", hobby, name);
         System.out.format("%s is a great pastime that is used by people all over the world.", hobby);
-
-        //int a = 2;
-        //int b = 5;
-
-        //System.out.println(a + " * " + b + " = " +  a * b);
     }
 }
