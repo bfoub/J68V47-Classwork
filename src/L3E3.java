0c8e57b9d9de85fe46ae6d2fda2ac6dd40744437
30044429 <30044429@nescol.ac.uk>
2023-09-13 15:28:18 +0100

    Lesson 3

--- src/L3E3.java
+++ src/L3E3.java
@@ -11,13 +11,12 @@ public class L3E3 {
         double j = input.nextDouble() / 100 / 12;
 
         System.out.print("Number of years: ");
-        int n = input.nextInt();
+        int n = input.nextInt() * 12;
 
         //double M = L(j / (1 - Math.pow((1+j) ,-n) ) );
-        double M = L(j / (1 - Math.pow((1 + j) , -n) ) )
+        double M = L * (j / (1 - Math.pow((1 + j) , -n) ) );
 
         System.out.print("Monthly payments for this loan: ");
         System.out.print(M);
-
     }
 }
